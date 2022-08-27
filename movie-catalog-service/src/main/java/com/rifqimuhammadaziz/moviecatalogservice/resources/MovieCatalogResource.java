0d2636b623e8752ea0150cac3d063d0281ba4f1f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-27 09:35:29 +0700

    push 3 microservices

---
+++ movie-catalog-service/src/main/java/com/rifqimuhammadaziz/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package com.rifqimuhammadaziz.moviecatalogservice.resources;
+
+import com.rifqimuhammadaziz.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Movie Example 1", "Test Desc", 4)
+        );
+    }
+}
