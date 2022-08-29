0ad10e480c47fab3331b6dbde2375635c77c6a68
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-29 10:31:46 +0700

    use rest template to call an external microservice api

--- movie-catalog-service/src/main/java/com/rifqimuhammadaziz/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/rifqimuhammadaziz/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,18 @@
 package com.rifqimuhammadaziz.moviecatalogservice.resources;
 
 import com.rifqimuhammadaziz.moviecatalogservice.models.CatalogItem;
+import com.rifqimuhammadaziz.moviecatalogservice.models.Movie;
+import com.rifqimuhammadaziz.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +20,26 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Movie Example 1", "Test Desc", 4)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("Movie001", 5),
+                new Rating("Movie002", 3),
+                new Rating("Movie003", 2)
         );
+
+        return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getMovieName(), "Test Desc", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+        // get all rated movie IDs
+
+        // for each movie ID, call movie info service and get details
+
+        // put them all
+
     }
 }
